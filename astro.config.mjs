91961f2b68c02b3087a95ebe838d384b64c09464
WebExpress <armando@webexpress.cl>
2026-05-11 00:45:09 -0400

    ♻️ Move landing/ to root (simplifica deploy Vercel + GitHub Pages)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 
+const isDeploy = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
-  site: 'https://webexpresschile.github.io',
-  base: '/WebExpress',
+  site: isDeploy ? 'https://webexpresschile.github.io' : 'https://landing-omega-flax.vercel.app',
+  base: isDeploy ? '/WebExpress' : '/',
   integrations: [tailwind(), react()],
   output: 'static',
   adapter: vercel(),
